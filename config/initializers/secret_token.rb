c44f12a062fa2108e87caee4fac7ff8e5abf5ddb
Mike Baldo <info@vergetone.com>
2013-12-30 16:56:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hbcv1::Application.config.secret_token = '77241abb3ff398b30f84da76889d3b8051eb654be8fdb07293c9548dbe443cef736f8f68505201541bd50e1e36b22a4796f0ac42fe21e4e139dcdfc2d8c79ed1'
